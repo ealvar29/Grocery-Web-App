0e485ce6290b800e262b017b2c9391c8acb2fd89
Eduardo <edalvarez2015@gmail.com>
2020-11-02 17:30:24 -0600

    First commit of project!

---
+++ Grocer.ly/Grocer.ly/Grocer.ly/Pages/Details.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Grocer.ly.Pages
+{
+    public class Details : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
