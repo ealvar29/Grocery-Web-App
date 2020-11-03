a66f3ace4b67a22fbde49bc4dfe1fd6339eff2a3
Eduardo <edalvarez2015@gmail.com>
2020-11-03 08:26:42 -0600

    Second commit - project almost done

--- Grocer.ly/Grocer.ly/Grocer.ly/Pages/Details.cshtml.cs
+++ Grocer.ly/Grocer.ly/Grocer.ly/Pages/Details.cshtml.cs
@@ -1,12 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using System.Collections.Generic;
+using Grocer.ly.Models;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Grocer.ly.Pages
 {
     public class Details : PageModel
     {
-        public void OnGet()
+        public List<GroceryItem> Foods = Inventory.ToList();
+        public GroceryItem CurrentFood;
+        
+        public void OnGet(string name)
         {
-            
+           CurrentFood = Foods.Find(x => x.Name.Trim().ToLower() == name.Trim().ToLower());
         }
     }
 }
--- Grocer.ly/Grocer.ly/Grocer.ly/Pages/Index.cshtml.cs
+++ Grocer.ly/Grocer.ly/Grocer.ly/Pages/Index.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Grocer.ly.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
@@ -10,6 +11,7 @@ namespace Grocer.ly.Pages
 {
     public class IndexModel : PageModel
     {
+        public List<GroceryItem> Foods = Inventory.ToList();
         private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger)
